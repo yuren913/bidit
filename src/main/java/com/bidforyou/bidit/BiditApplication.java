9b45ecf846cbef06b6ecd37444db2f48edd0cfcd
Zhang Min  (NCS) <zhangm@ncs.com.sg>
2018-07-26 09:53:43 +0800

    init

---
+++ src/main/java/com/bidforyou/bidit/BiditApplication.java
@@ -0,0 +1,12 @@
+package com.bidforyou.bidit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiditApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bidforyou/bidit/BiditApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bidforyou.bidit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiditApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
